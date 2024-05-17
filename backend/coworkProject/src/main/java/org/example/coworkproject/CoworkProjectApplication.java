f3409805b4306b56c3b249a67c593b606ee59164
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-17 18:12:44 -0300

    Push Backend

---
+++ backend/coworkProject/src/main/java/org/example/coworkproject/CoworkProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.coworkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoworkProjectApplication.class, args);
+    }
+
+}
