8d533eeb43abcbcec5b50479d9bdb60ed26125a5
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-26 16:23:58 -0300

    Update SecurityConfig.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/configuration/security/SecurityConfig.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/configuration/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.coworkproject.configuration.security;
 
 import lombok.RequiredArgsConstructor;
 import org.example.coworkproject.configuration.security.jwt.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -22,8 +23,10 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final AuthenticationProvider authenticationProvider;
-    private final JwtFilter jwtFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securiityFilterChain(HttpSecurity httpSecurity) throws Exception {
