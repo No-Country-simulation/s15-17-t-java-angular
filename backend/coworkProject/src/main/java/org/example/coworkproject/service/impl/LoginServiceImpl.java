f04807e583ae672b3e263300e90270dcebe72335
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-26 16:25:21 -0300

    Update LoginServiceImpl.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/service/impl/LoginServiceImpl.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/service/impl/LoginServiceImpl.java
@@ -3,7 +3,6 @@ package org.example.coworkproject.service.impl;
 import org.example.coworkproject.configuration.security.jwt.JwtService;
 import org.example.coworkproject.dto.request.LoginRequestDTO;
 import org.example.coworkproject.dto.response.LoginResponseDTO;
-import org.example.coworkproject.exception.CustomAuthenticationException;
 import org.example.coworkproject.exception.MyException;
 import org.example.coworkproject.repository.UserRepository;
 import org.example.coworkproject.service.LoginService;
@@ -11,26 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
-    private JwtService jwtService;
-    private AuthenticationManager authenticationManager;
-
     @Autowired
-    public LoginServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
-    }
-
+    private UserRepository userRepository;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public LoginResponseDTO login(LoginRequestDTO requestDTO) throws MyException {
         try {
@@ -54,8 +45,7 @@ public class LoginServiceImpl implements LoginService {
                 .email(user.getEmail())
                 .id_user(user.getId_user())
                 .token(jwtToken)
-                .name(user.getName())
-                .lastName(user.getLastName())
+                .fullName(user.getFullName())
                 .build();
     }
 }
