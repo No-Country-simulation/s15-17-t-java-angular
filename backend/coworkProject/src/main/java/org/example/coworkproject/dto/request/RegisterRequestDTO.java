5f4c399cc7bd5ae76d9109576d6ae6e227e0c5fe
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-29 17:21:45 -0300

    Update RegisterRequestDTO.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/RegisterRequestDTO.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package org.example.coworkproject.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,10 @@ public class RegisterRequestDTO {
 
     @NotBlank(message = "Password is required.")
     @Size(min = 6, message = "Password must be at least 6 characters.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain at least one uppercase letter, one number, and one special character."
+    )
     private String password;
 
     @NotBlank(message = "Full name is required.")
