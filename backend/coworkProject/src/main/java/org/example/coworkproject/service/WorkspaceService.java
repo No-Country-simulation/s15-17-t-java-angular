4a5f56810936225fd905dd2c00b10957192215b3
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-29 16:50:34 -0300

    Update WorkspaceService.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/service/WorkspaceService.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/service/WorkspaceService.java
@@ -1,72 +1,23 @@
 package org.example.coworkproject.service;
 
-import org.example.coworkproject.entity.QualitiesEntity;
+import org.example.coworkproject.dto.request.WorkspaceRequestDTO;
+import org.example.coworkproject.dto.response.WorkspaceResponseDTO;
 import org.example.coworkproject.entity.WorkspaceEntity;
-import org.example.coworkproject.repository.QualitiesRepository;
-import org.example.coworkproject.repository.WorkspaceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class WorkspaceService {
+public interface WorkspaceService {
+    List<WorkspaceResponseDTO> getAllWorkspaces();
 
-    @Autowired
-    private WorkspaceRepository workspaceRepository;
+    WorkspaceResponseDTO getWorkspaceById(Long id);
 
-    @Autowired
-    private QualitiesRepository qualitiesRepository;
+    WorkspaceResponseDTO addQualityToWorkspace(Long id_workspace, Long idQuality);
 
-    public List<WorkspaceEntity> getAll() {
-        return workspaceRepository.findAll();
-    }
+    WorkspaceResponseDTO removeQualityFromWorkspace(Long id_workspace, Long id_quality);
+    WorkspaceResponseDTO createWorkspace(WorkspaceRequestDTO workspaceRequestDTO);
 
-    public WorkspaceEntity getById(Long id) {
-        return workspaceRepository.findById(id).orElse(null);
-    }
+    WorkspaceResponseDTO updateWorkspace(Long id, WorkspaceRequestDTO updatedWorkspaceRequestDTO);
 
-    public WorkspaceEntity addQualityToWorkspace(Long idWorkspace, Long idQuality){
-        WorkspaceEntity workspace = workspaceRepository.findById(idWorkspace).orElse(null);
-        QualitiesEntity quality = qualitiesRepository.findById(idQuality).orElse(null);
-
-        if(workspace != null && quality != null){
-            if(!workspace.getQualities().contains(quality)){
-                workspace.getQualities().add(quality);
-                quality.getWorkspaces().add(workspace);
-                workspaceRepository.save(workspace);
-                qualitiesRepository.save(quality);
-
-                return workspace;
-            }
-
-            return null;
-        }
-        return null;
-    }
-
-    public WorkspaceEntity create(WorkspaceEntity body){
-        return workspaceRepository.save(body);
-    }
-
-    public Optional<WorkspaceEntity> update(Long id, WorkspaceEntity updatedQuality) {
-        return workspaceRepository.findById(id).map(existingWorkspace -> {
-            existingWorkspace.setCity(updatedQuality.getCity());
-            existingWorkspace.setCountry(updatedQuality.getCountry());
-            existingWorkspace.setIsPublic(updatedQuality.getIsPublic());
-            existingWorkspace.setAddress(updatedQuality.getAddress());
-            existingWorkspace.setPrice(updatedQuality.getPrice());
-            return workspaceRepository.save(existingWorkspace);
-        });
-    }
-
-    public WorkspaceEntity delete(Long id) {
-        Optional<WorkspaceEntity> workspace = workspaceRepository.findById(id);
-        if (workspace.isPresent()) {
-            workspaceRepository.delete(workspace.get());
-            return workspace.get();
-        }
-        return null;
-    }
+    WorkspaceResponseDTO deleteWorkspace(Long id);
 }
