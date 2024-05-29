1ad835a3086bdf19acceaa7441b6e23db0d64e2b
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-29 16:50:28 -0300

    Update QualitiesService.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/service/QualitiesService.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/service/QualitiesService.java
@@ -1,47 +1,25 @@
 package org.example.coworkproject.service;
 
+import org.example.coworkproject.dto.request.QualitiesRequestDTO;
+import org.example.coworkproject.dto.response.QualitiesResponseDTO;
+import org.example.coworkproject.dto.response.WorkspaceResponseDTO;
 import org.example.coworkproject.entity.QualitiesEntity;
-import org.example.coworkproject.entity.WorkspaceEntity;
-import org.example.coworkproject.repository.QualitiesRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class QualitiesService {
-    private final QualitiesRepository qualitiesRepository;
+public interface QualitiesService {
 
-    public QualitiesService(QualitiesRepository qualitiesRepository) {
-        this.qualitiesRepository = qualitiesRepository;
-    }
 
-    public List<QualitiesEntity> getAll() {
-        return qualitiesRepository.findAll();
-    }
+    List<QualitiesResponseDTO> getAllQualities();
 
-    public QualitiesEntity getById(Long id) {
-        return qualitiesRepository.findById(id).orElse(null);
-    }
+    QualitiesResponseDTO getQualityById(Long id_qualities);
 
-    public QualitiesEntity create(QualitiesEntity body){
-        return qualitiesRepository.save(body);
-    }
+    QualitiesResponseDTO createQuality(QualitiesRequestDTO qualitiesRequestDTO);
 
-    public Optional<QualitiesEntity> update(Long id, QualitiesEntity updatedQuality) {
-        return qualitiesRepository.findById(id).map(existingQuality -> {
-            existingQuality.setNombre(updatedQuality.getNombre());
-            return qualitiesRepository.save(existingQuality);
-        });
-    }
+    QualitiesResponseDTO updateQuality(Long id_qualities, QualitiesRequestDTO updatedQualityRequestDTO);
 
-    public QualitiesEntity delete(Long id) {
-        Optional<QualitiesEntity> quality = qualitiesRepository.findById(id);
-        if (quality.isPresent()) {
-            qualitiesRepository.delete(quality.get());
-            return quality.get();
-        }
-        return null;
-    }
+    QualitiesResponseDTO deleteQuality(Long id_qualities);
 
+    List<WorkspaceResponseDTO> getWorkspacesByQuality(Long id_quality);
 }
