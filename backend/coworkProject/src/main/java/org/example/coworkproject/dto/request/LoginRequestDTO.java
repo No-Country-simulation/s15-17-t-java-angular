f38aad933613852f94ddb58c35e29d1678b2b89f
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-29 17:21:41 -0300

    Update LoginRequestDTO.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/LoginRequestDTO.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/LoginRequestDTO.java
@@ -2,6 +2,7 @@ package org.example.coworkproject.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,5 +22,9 @@ public class LoginRequestDTO {
 
     @NotBlank(message = "Password is required.")
     @Size(min = 6, message = "Password must be at least 6 characters.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain at least one uppercase letter, one number, and one special character."
+    )
     private String password;
 }
